37b63b7776797b76027bd64f59dfb0b1f0006cdb
Judith <104207025+judithlk@users.noreply.github.com>
2024-08-03 08:27:20 +0100

    Search function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
-import Listings from "@/components/Listings"
+"use client"
+
+import Header from "@/components/Header"
+import Featured from "@/components/Featured"
+import Detailed from "@/components/Detailed"
+import SearchResult from "@/components/SearchResults"
 
 export default function Home() {
+
   return(
-    <Listings />
+    <>
+      <Header />
+      <main className="bg-bg-mid md:bg-bg-one bg-center bg-fixed bg-cover bg-no-repeat relative z-0 py-2 space-y-10">
+        <div
+          className="absolute inset-0 z-[-1]"
+          style={{ backgroundColor: "rgba(230, 230, 230, 0.9)" }}
+        />
+        <SearchResult />
+        <Featured />
+        <Detailed />
+      </main>
+    </>
   )
 }
---
+++ src/components/SearchResults.tsx
@@ -0,0 +1,64 @@
+"use client"
+
+import { useState, useEffect } from "react"
+import { useSearchParams } from "next/navigation"
+
+import { useGetListingsQuery } from "@/redux/services/listing.service"
+
+import DetailCard from "@/components/DetailCard"
+
+export default function SearchResult() {
+    const searchParams = useSearchParams();
+    const search = searchParams.get('search') || "";
+    const { data: listings = [], error, isLoading } = useGetListingsQuery();
+    const [filteredResults, setFilteredResults] = useState([]);
+  
+    useEffect(() => {
+      if (listings.length > 0) {
+        if (search) {
+          const query = search.toLowerCase();
+          const filtered = listings.filter((listing: any) => 
+            listing.title.toLowerCase().includes(query) || 
+            listing.description.toLowerCase().includes(query) ||
+            listing.price.toString().includes(query) ||
+            listing.facilities.toString().includes(query)
+          );
+          setFilteredResults(filtered);
+        } else {
+          setFilteredResults(listings);
+        }
+      }
+    }, [search, listings]);
+  
+    if (isLoading) {
+      return <div className={`container mx-auto p-4 ${search == "" ? "hidden" : "block"}`}>Loading...</div>;
+    }
+  
+    if (error) {
+      return <div className="container mx-auto p-4">Error!</div>;
+    }
+  
+    const results = filteredResults.length > 0 ? filteredResults : null;
+
+    return (
+        <div className={`w-[90%] m-auto space-y-2 ${search == "" ? "hidden" : "block"}`}>
+            Search results
+          {results?.map((listing:any) => (
+            <DetailCard
+              key={listing.id}
+              image={listing.image}
+              title={listing.title}
+              description={listing.description}
+              cardist={listing.car_dist}
+              walkdist={listing.walk_dist}
+              traindist={listing.train_dist}
+              facilities={listing.facilities}
+              price={listing.price}
+              showwalk={listing.walk_dist > 0}
+              showtrain={listing.train_dist > 0}
+              showcar={listing.car_dist > 0}
+            />
+          ))}
+        </div>
+      );
+}
